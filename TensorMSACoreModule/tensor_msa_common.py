6a874f12114020b6e3c0025e901192d589fd2224
tmddno1 <tmddno1@gmail.com>
2016-09-04 03:31:03 +0900

    account error test

--- TensorMSACoreModule/tensor_msa_common.py
+++ TensorMSACoreModule/tensor_msa_common.py
@@ -11,6 +11,7 @@ from tensorflow.contrib import learn
 import os
 
 
+# last test too late
 # commit account test 222
 # delete the accoutn but still using a wrong account
 # contribution count test
