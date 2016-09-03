00765251a4e3fbfe2bc629449e1130fd919015d1
tmddno1@naver.com <tmddno1@naver.com>
2016-09-03 14:18:28 -0400

    account is wrong

--- TensorMSACoreModule/tensor_msa_common.py
+++ TensorMSACoreModule/tensor_msa_common.py
@@ -10,7 +10,7 @@ import numpy as np
 from tensorflow.contrib import learn
 import os
 
-
+# commit test on ohter machines
 # commit account test
 # delete the accoutn but still using a wrong account
 # contribution count test
