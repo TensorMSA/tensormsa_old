53e064e2eeaf1ea08d0d8c4078cbef6a2390a420
tmddno1@naver.com <tmddno1@naver.com>
2016-09-11 19:55:49 -0400

    jango static file deploy path change

--- TensorMSA/settings.py
+++ TensorMSA/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR , 'tfmsaview/')
+STATIC_ROOT = os.path.join(BASE_DIR , 'tfmsaview/dist')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "tfmsaview/static"),
