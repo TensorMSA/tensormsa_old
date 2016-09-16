8867c0baf0f1c9e7c00c1ce58477b0fbac03ee32
healess <healess1@gmail.com>
2016-09-17 00:06:13 +0900

    Fix bug about ES2015 configuration.

--- tfmsaview/static/webpack.config.js
+++ tfmsaview/static/webpack.config.js
@@ -8,14 +8,18 @@ module.exports = {
     debug: true,
     output: {
         path: __dirname,
-        filename: './dist/TensorMSA.js'
+        filename: './../dist/TensorMSA.js'
     },
     module: {
         loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader'
+            { 
+                test: /\.jsx?$/,         // Match both .js and .jsx files
+                exclude: /node_modules/, 
+                loader: "babel", 
+                query:
+                {
+                    presets:['es2015','react']
+                }
             }
         ]
     }
