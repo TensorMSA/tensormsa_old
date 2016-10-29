d2f76becd0943f486de353cbc642512ef7b7a59f
seungwookim <tmddno1@naver.com>
2016-10-29 14:32:58 +0000

    django - view : sync webpack location and static path

--- tfmsaview/static/dist/TensorMSA.js
+++ tfmsaview/static/dist/TensorMSA.js
@@ -16029,7 +16029,8 @@
 	  if (x === y) {
 	    // Steps 1-5, 7-10
 	    // Steps 6.b-6.e: +0 != -0
-	    return x !== 0 || 1 / x === 1 / y;
+	    // Added the nonzero y check to make Flow happy, but it is redundant
+	    return x !== 0 || y !== 0 || 1 / x === 1 / y;
 	  } else {
 	    // Step 6.a: NaN == NaN
 	    return x !== x && y !== y;
@@ -28053,7 +28054,7 @@
 	});
 	exports.default = {
 	    getApiServerUrl: function getApiServerUrl() {
-	        return ("http://52.78.19.96:8989/");
+	        return (undefined);
 	    }
 	};
 
