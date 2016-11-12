2b78e956bdcb648dc52b46db2397c7c8a9c43961
seungwookim <tmddno1@naver.com>
2016-11-12 16:36:56 +0000

    clean : remove unnecessary print code

--- tfmsarest/views/cnn_eval.py
+++ tfmsarest/views/cnn_eval.py
@@ -33,9 +33,7 @@ class ConvNeuralNetEval(APIView):
         - desc : evaluate train result
         """
         try:
-            print(request.body)
             jd = jc.load_obj_json(str(request.body, 'utf-8'))
-            print(jd.samplenum)
             result = eval_conv_network(nnid, jd.samplenum, jd.samplemethod)
             return_data = {"status": "ok", "result": result}
             return Response(json.dumps(return_data))
