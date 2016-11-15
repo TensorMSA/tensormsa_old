dabcb7962b442c6bcdc33a0630d6465de808cc7a
seungwookim <tmddno1@naver.com>
2016-11-15 02:46:00 +0000

    UPDATE : NetWork progress status getter,setter

--- tfmsacore/netconf/__init__.py
+++ tfmsacore/netconf/__init__.py
@@ -4,3 +4,5 @@ from .nn_config_manager import load_conf, save_conf, load_ori_conf, chk_conf, re
 from .nn_model_manager import load_trained_data, save_trained_data, chk_trained_data,remove_trained_data
 from .nn_format_manager import chk_format,load_format,load_ori_format,remove_format,save_format
 from .nn_train_manager import get_train_acc,get_train_loss,post_train_acc,post_train_loss,delete_train_acc,delete_train_loss
+from .nn_flag_manager import set_off_data,set_off_data_conf,set_off_net_conf,set_off_net_vaild,set_off_train,\
+    set_on_data,set_on_data_conf,set_on_net_conf,set_on_net_vaild,set_on_train
