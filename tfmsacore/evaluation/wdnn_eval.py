3804fc523bda408f34c25731d017947efb04ef28
jeehyunPaik <jeehyuun100@poscoict.com>
2016-12-06 12:33:14 +0000

    wdnn comment cleansing

--- tfmsacore/evaluation/wdnn_eval.py
+++ tfmsacore/evaluation/wdnn_eval.py
@@ -19,7 +19,7 @@ from sklearn.preprocessing import LabelEncoder
 
 class wdnn_eval(WdnnCommonManager):
     def __init__(self):
-        print("############# Create wdnn_eval")
+        utils.tfmsa_logger("Create Wdnn eval ")
         WdnnCommonManager.__init__(self)
 
     def wdd_eval(self, nnid, filename=None):
@@ -31,24 +31,14 @@ class wdnn_eval(WdnnCommonManager):
         """
         try:
             utils.tfmsa_logger("((3.1 Wide & Deep Network Eval Start)) ## "+ nnid + "##")
-            #print(nnid)
-
-            #get network info from postgres
             json_string = WdnnCommonManager.get_all_info_json_by_nnid(self, nnid=nnid)
-            #print("WdnnCommonManager.get_json_by_nnid after")
-            #print(json_string)
-            #print(type(json_string))
+
             database = json_string['dir']
             table_name = json_string['table']
-            #print(database)
-            #print(table_name)
             json_string_desc = netconf.load_ori_format(nnid)
             json_ob = json.loads(json_string_desc)  #get datadesc format
             model_dir = str(json_string['query'])
-            tt = json_ob['cell_feature']
-            #json_ob = json.loads(json_object)
-            #t_label = json_ob["label"]
-            ##label_column = list(t_label.keys())[0]
+
             label_column = list(json_ob["label"].keys())[0]
 
             _label_list = json_string['datasets']
@@ -56,28 +46,15 @@ class wdnn_eval(WdnnCommonManager):
             utils.tfmsa_logger("((3.1 Wide & Deep Network Eval Start)) ## Model_Dir : " + model_dir )
             utils.tfmsa_logger("((3.1 Wide & Deep Network Eval Start)) ## Label Column " + label_column + " Label : " + _label_list)
 
-            #print(_label_list)
-
             label_list = eval(_label_list)
-            #print("##########predict label list ########  " + str(label_list))
-
-
 
             wdnn_model = WdnnCommonManager.wdnn_build(self, nnid, model_dir, False)
 
-
-
-
             if filename == None:
-                #limit_no = 100
-                #print(
-                #    "((2.Get Dataframe from Hbase)) ##Start## (" + database + " , " + table_name + " , " + label_column + ")")
                 df, last_key = data.DataMaster().query_data("test_schema_" +  database, table_name, "a", use_df = True, limit_cnt=-1, with_label=label_column)
                 utils.tfmsa_logger("((3.1 Wide & Deep Network Eval Start)) ## Get Test Schema Data Count" + str(len(df)) + "LastKey : " + str(last_key))
-                #print(
-                #    "((2.Get Dataframe from Hbase)) ##End## (" + database + " , " + table_name + " , " + label_column + " , " + str(
-                #        limit_no) + ")")
             else:
+                #Don't use this pat
                 print("((2.Get Dataframe from CSV)) ##Start## (" + nnid + " , " + filename + ")")
                 file_path = settings.FILE_ROOT + "/predict/" + nnid + "/" + filename
                 print("((2.Get Dataframe from CSV)) ##filePath## (" + file_path + ")")
@@ -87,7 +64,6 @@ class wdnn_eval(WdnnCommonManager):
                     # names=COLUMNS,
                     skipinitialspace=True,
                     engine="python")
-                # add label feature for wdnn netowrk
                 df['label'] = (df[label_column].apply(lambda x: "Y" in x)).astype(int)
 
             results = {}
@@ -96,7 +72,7 @@ class wdnn_eval(WdnnCommonManager):
             df['predict_label'] = list(predict_results)
 
             #print(df['predict_label'] )
-            print("make label encorder function ######################")
+            utils.tfmsa_logger("make label encorder function ######################")
             #
             #Decode Label(int) to Label(str)
             le = LabelEncoder()
@@ -104,17 +80,23 @@ class wdnn_eval(WdnnCommonManager):
             lable_decoder_func = lambda x: le.inverse_transform([x])
             df['predict_label'] = df['predict_label'].map(lable_decoder_func).astype("str")
 
+            label_value = list(df['label'].unique())
+            label_encode = le.inverse_transform(label_value)
+            utils.tfmsa_logger("((3.1 Wide & Deep Network Predict)) ## Label Convert ##" + str(label_value) + "DECODE ---> " + str(label_encode))
+
             #results_acc table manager class create
             acc_result_obj = AccStaticResult()
-
+            log_count = 0
             for value in df.iterrows():
-                print("Inside start row by row")
-                print(type(value))
+                log_count +=1
+                #print("Inside start row by row")
+                #print(type(value))
                 _row = value[1]
                 ori_label = _row[label_column]
                 predict_label = str(eval(_row["predict_label"])[0])#str(list(_row["predict_label"])[0])
                 #predict_label = le.inverse_transform(_predict_label)
-                print(str(ori_label) + "-------> " + str(predict_label))
+                if (log_count%20 == 0):
+                    utils.tfmsa_logger("((3.1 Wide & Deep Network Eval Columns Insert)) "+ str(ori_label) + "------->" + str(predict_label))
                 acc_result_obj = AccEvalCommon(nnid).set_result(acc_result_obj, ori_label,predict_label)
             return results
         except Exception as e:
--- tfmsacore/netcommon/wdnn_common.py
+++ tfmsacore/netcommon/wdnn_common.py
@@ -23,30 +23,24 @@ WDNN NETWORK COMMON CLASS
 """
 class WdnnCommonManager:
     def __init__(self):
-        print("############# WdnnCommonManager ")
+        utils.tfmsa_logger("Create Wdnn common ")
 
     def input_fn(self, df, nnid):
         """Wide & Deep Network input tensor maker
             V1.0    16.11.04    Initial
                 :param df : dataframe from hbase
-                :param nnid
+                :param df, nnid
                 :return: tensor sparse, constraint """
         try:
             utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## START ##")
             ##Make List for Continuous, Categorical Columns
             CONTINUOUS_COLUMNS = []
             CATEGORICAL_COLUMNS = []
-
             ##Get datadesc Continuous and Categorical infomation from Postgres nninfo
-            print("modified 16.11.23")
             json_string = self.get_json_by_nnid(nnid)
-            #json_object_temp = json_string['datadesc']
-            #json_object = json.loads(json_object_temp) # should be change meaning confused
             json_object = json_string
-
             utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## JSON CELL FEATURE LOADS ##")
             j_feature = json_object['cell_feature']
-
             for cn, c_value in j_feature.items():
               if c_value["column_type"] == "CATEGORICAL":
                   CATEGORICAL_COLUMNS.append(cn)
@@ -54,66 +48,44 @@ class WdnnCommonManager:
                   CONTINUOUS_COLUMNS.append(cn)
               elif c_value["column_type"] =="CATEGORICAL_KEY":
                   CATEGORICAL_COLUMNS.append(cn)
-
             utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## SPARSE TENSOR ##" + "".join(CATEGORICAL_COLUMNS))
             utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## REAL VALUE TENSOR ##" + "".join(CONTINUOUS_COLUMNS))
-            #print("((3.1 Wide & Deep Network Make Tensor)) ## SPARSE TENSOR ##", CATEGORICAL_COLUMNS)
-            #print("((3.1 Wide & Deep Network Make Tensor)) ## REAL VALUE TENSOR ##", CONTINUOUS_COLUMNS)
-
             # Check Continuous Column is exsist?
             if len(CONTINUOUS_COLUMNS)>0 :
-                print(CONTINUOUS_COLUMNS)
-                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CONTINUES")
-                #print("((3.1 Wide & Deep Network Make Tensor)) ## IF CONTINUES")
-                #for k in CONTINUOUS_COLUMNS:
-                #    print(k)
-                #    print(type(df))
-                #    print(df[k].values)
+                #print(CONTINUOUS_COLUMNS)
+                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CONTINUES : " + str(CONTINUOUS_COLUMNS))
                 continuous_cols = {k: tf.constant(df[k].values) for k in CONTINUOUS_COLUMNS}
-
             # Check Categorical Column is exsist?
             if len(CATEGORICAL_COLUMNS) > 0 :
-                #print("((3.1 Wide & Deep Network Make Tensor)) ## IF CATEGORICAL")
-                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CATEGORICAL")
-                #for k in CATEGORICAL_COLUMNS:
-                    #print(k)
-                    #print("df should ne wrong?")
-                    #print(df[k].size)
+                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CATEGORICAL(SPARSE TENSOR) ")
                 for k in CATEGORICAL_COLUMNS:
                     df[k] = df[k].astype('str')
-
                 categorical_cols = {k: tf.SparseTensor(
                   indices=[[i, 0] for i in range(df[k].size)],
                   values=df[k].values,
                   shape=[df[k].size, 1])
                                   for k in CATEGORICAL_COLUMNS}
-                print("####### sparse tensor input fn END")
+                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ####### CATEGORICAL(SPARSE TENSOR) END")
             # Merges the two dictionaries into one.
-            utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## SAPRSE TENSOR INPUT ##")
-            #print("((3.1 Wide & Deep Network Make Tensor)) ## SAPRSE TENSOR INPUT ##")
             feature_cols = {}
             if(len(CONTINUOUS_COLUMNS)>0):
-                #print("((3.1 Wide & Deep Network Make Tensor)) ## IF CONTINUE ADD LIST")
-                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CONTINUE ADD LIST")
+                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CONTINUE ADD LIST : " + str(CONTINUOUS_COLUMNS))
                 feature_cols.update(continuous_cols)
             if len(CATEGORICAL_COLUMNS) > 0:
-                #print("((3.1 Wide & Deep Network Make Tensor)) ## IF CATEGORICAL ADD LIST")
-                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CATEGORICAL ADD LIST")
+                utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## IF CATEGORICAL ADD LIST" + str(CATEGORICAL_COLUMNS))
                 feature_cols.update(categorical_cols)
 
             #Get label distinct list from postgres 16.12.04
             json_string = WdnnCommonManager.get_all_info_json_by_nnid(self, nnid=nnid)
             _label_list = json_string['datasets']
-            #print(_label_list)
-
             utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## Make Label List ##")
             label_list = eval(_label_list)
             le = LabelEncoder()
             le.fit(label_list)
             lable_encoder_func = lambda x: le.transform([x])
             df['label'] = df['label'].map(lable_encoder_func).astype(int)
-            #print(df['label'])
-
+            label_encode = le.transform(label_list)
+            utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## Label Convert ##" + str(label_list) + "ENCODE ---> " + str(label_encode))
             label = tf.constant(df["label"].values)
             utils.tfmsa_logger("((3.1 Wide & Deep Network Make Tensor)) ## END ##")
             return feature_cols, label
@@ -126,41 +98,16 @@ class WdnnCommonManager:
         :param nnid
         :return: json string """
 
-        utils.tfmsa_logger("((get_json_by_nnid)) ## START##")
-        print(nnid)
-        #print("get_json_networkid########")
-        #print("((get_json_by_nnid)) ## START##")
-        # result = netconf.load_ori_format()(nnid, request.body)
-        # result_temp = netconf.get_network_config(nnid)
-        #
-        # datadesc = netconf.load_ori_format(nnid)
-        # print(datadesc)
-        # result_datadesc_source = json.loads(datadesc)
-        # # result_datadesc_source = eval(result_temp["datadesc"])
-        # # result_temp = netconf.get_network_config(nnid)
-        # # result_datadesc_source = eval(result_temp)
-        #
-        # # print(str(request.body, 'utf-8'))
-        # print("after get data")
-        # print(result_datadesc_source)
-        # result = dict()
-        # result1 = result_datadesc_source["cell_feature"]
-        # result2 = result_datadesc_source["label"]
-
         datadesc = netconf.load_ori_format(nnid)
         result = json.loads(datadesc)
-        print(result)
-        #result = netconf.get_network_config(nnid)
-        utils.tfmsa_logger("((get_json_by_nnid)) ## END##")
+        utils.tfmsa_logger("((Wdnn_common_model_get_json_by_nnid)) ## END##")
         return result
     def get_all_info_json_by_nnid(self,nnid):
         """get network configuration info json
         :param nnid
         :return: json string """
-        utils.tfmsa_logger("((get_all_info_json_by_nnid)) ## START##")
-        print(nnid)
         result = netconf.get_network_config(nnid)
-        utils.tfmsa_logger("((get_json_by_nnid)) ## END##")
+        utils.tfmsa_logger("((Wdnn_common_model_get_json_by_nnid)) ## END##")
         return result
 
     def wdnn_build(self,nnid, model_dir = "No", train=True):
@@ -170,71 +117,39 @@ class WdnnCommonManager:
             :param train : train or predict
             :return: tensorflow network model """
         try:
-            # need json, model_dir
-            #utils.tfmsa_logger("((1.Make WDN Network Build)) start wddd build (" + nnid + ")")
-            #print("((1.Make WDN Network Build)) start wddd build (" + nnid + ")")
-            #json_string = self.get_json_by_nnid(nnid)
-            #print("get json string in wdnn builder ####")
-            #print(json_string)
-
-            #json_object = json.loads(json.dumps(json_string["datadesc"])) #3.5 fixed 16.11.02
-            #json_object = json.loads(json_string["datadesc"])
-            #print("get json string in wdnn builder ####")
-            # load NN conf form db
-            #utils.tfmsa_logger("[4]load net conf form db")
-
-            #conf = netconf.load_conf(nnid)
-            #hidden_layers_value = conf.layer
-            #hidden_layers_value2 = conf["layer"]
-            #print("((1.Make WDN Network Build)) config load " + str(hidden_layers_value2))
-            #print("((1.Make WDN Network Build)) set up Hidden Layers ("+ str(hidden_layers_value),'utf-8' + ")")
-
-            print("COMMON_WDNN_build")
-
+            utils.tfmsa_logger("((0.0 Wide & Deep Network wdnn build))")
             conf,hidden_layers_value,json_object, label_object  = self.get_init_info_wdnn(nnid)
 
-
             #Check Train or Predict
-
             if(train):
                 #model_dir = settings.HDFS_MODEL_ROOT + "/"+nnid + "/"+tempfile.mkdtemp().split("/")[2]
                 model_dir = settings.HDFS_MODEL_ROOT + "/" + nnid
             else:
                 if(model_dir != "No"):
                     model_dir = model_dir
-            print("((1.Make WDN Network Build)) set up WDNN directory("+nnid +") ---> " + model_dir)
+            utils.tfmsa_logger("((1.Make WDN Network Build)) set up WDNN directory("+nnid +") ---> " + model_dir)
             label_cnt = len(list(label_object))
-            print("((1.Make WDN Network Build)) set up WDNN label count("+nnid +") ---> " + str(label_cnt))
+            utils.tfmsa_logger("((1.Make WDN Network Build)) set up WDNN label count("+nnid +") ---> " + str(label_cnt))
             # continuous, categorical and embeddingforCategorical(deep) list
             featureColumnCategorical = {}
             featureColumnContinuous = {}
             featureDeepEmbedding={}
-            #print("before json convert")
-            #print(json_object) #cross_cell
-            print("before Cell feature")
-            print(json_object)
-            print(type(json_object))
             j_feature = json_object["cell_feature"]
-            print(j_feature)
-            print("after Cell feature")
-            print("((1.Make WDN Network Build)) set up Hidden Layers (" + str(hidden_layers_value), 'utf-8' + ")")
+            utils.tfmsa_logger("((1.Make WDN Network Build)) Get Column DataType (" + nnid + ") ---> " + str(j_feature))
+            utils.tfmsa_logger("((1.Make WDN Network Build)) set up Hidden Layers (" + str(hidden_layers_value) + ")")
             for cn, c_value in j_feature.items(): #change 3.5python
-                print("((1.Make WDN Network Build)) first get feature columns " + str(c_value["column_type"]),'utf-8')
+                utils.tfmsa_logger("((1.Make WDN Network Build)) first get feature columns [" + cn + "]:  " + str(c_value["column_type"]))
 
                 if c_value["column_type"] == "CATEGORICAL":
                     featureColumnCategorical[cn] = tf.contrib.layers.sparse_column_with_hash_bucket(
                         cn, hash_bucket_size=1000)
                 elif c_value["column_type"] == "CATEGORICAL_KEY":
-                    print("((1.Make WDN Network Build)) categorical_key add ")
-                    print(c_value["keys"])
                     featureColumnCategorical[cn] = tf.contrib.layers.sparse_column_with_keys(column_name=cn,keys=c_value["keys"])
-                    print("((1.Make WDN Network Build)) categorical_key add end ")
                 elif c_value["column_type"] == "CONTINUOUS": #CONTINUOUS
-                    print("((1.Make WDN Network Build)) CONTINUOUS add ")
                     featureColumnContinuous[cn] = tf.contrib.layers.real_valued_column(cn)
             # embedding column add
             for key, value in featureColumnCategorical.items(): #3.5python
-                print("((1.Make WDN Network Build)) Categorical Embedding add ")
+                utils.tfmsa_logger("((1.Make WDN Network Build)) Categorical Embedding add ["+ str(key)+"]")
                 featureDeepEmbedding[key] = tf.contrib.layers.embedding_column(value, dimension=8)
 
             wide_columns = []
@@ -246,7 +161,7 @@ class WdnnCommonManager:
             if 'cross_cell' in json_object: #json_object.has_key('cross_cell'):
                 j_cross = json_object["cross_cell"]
                 for jc, values in j_cross.items():
-                    print("((1.Make WDN Network Build)) Cross rows " + str(values) )
+                    utils.tfmsa_logger("((1.Make WDN Network Build)) Cross rows " + str(values) )
                     for c_key, c_value in values.items(): #3.5python
                         cross_col1.append(featureColumnCategorical[c_value])
                     wide_columns.append(tf.contrib.layers.crossed_column(cross_col1,hash_bucket_size=int(1e4)))
@@ -256,57 +171,46 @@ class WdnnCommonManager:
             if 'Transformations' in json_object: #json_object.has_key('Transformations'):
                 j_boundaries = json_object["Transformations"]
                 for jc, values in j_boundaries.items(): #3.5python
-                    print("((1-1.Make WDN Network Build)) TransForm Columns " + str(values))
+                    utils.tfmsa_logger("((1-1.Make WDN Network Build)) TransForm Columns " + str(values))
                     trans_col_name = values["column_name"]
                     trans_boundaries = values["boundaries"]
-                    print("((1-1 get age columns  )) ")
-                    print(type(featureColumnContinuous[trans_col_name]))
+                    utils.tfmsa_logger("((1-1 get age columns  )) ")
+                    utils.tfmsa_logger(type(featureColumnContinuous[trans_col_name]))
                     rvc = featureColumnContinuous[trans_col_name]
 
-                    #print("((1-1 transform cell parameters )) key : " + str(jc) +" --->  "+ trans_col_name + ":" + trans_boundaries)
                     transfomation_col[jc] = tf.contrib.layers.bucketized_column(featureColumnContinuous[trans_col_name],trans_boundaries)
                     wide_columns.append(tf.contrib.layers.bucketized_column(featureColumnContinuous[trans_col_name],trans_boundaries))
-                    print("((1-1 transform tensor insert))")
+                    utils.tfmsa_logger("((1-1 transform tensor insert))")
 
             deep_columns = []
             for realTensor in featureColumnContinuous:
                 deep_columns.append(featureColumnContinuous[realTensor])
 
-            # categorucal colums change to embedTensor for Deep
             for embeddingTensor in featureDeepEmbedding:
                 deep_columns.append(featureDeepEmbedding[embeddingTensor])
-    #'wide', 'deep', 'wide_n_deep'
-            network_type = flags.DEFINE_string
-
-            #make lable count
-
-
 
             if FLAGS.model_type == "wide_n_deep":
-                print("###################wide and deep network##############")
+                utils.tfmsa_logger("((1-1 wide_n_deep Selected)############)")
                 m = tf.contrib.learn.DNNLinearCombinedClassifier(
                     model_dir=model_dir,
                     linear_feature_columns=wide_columns,
                     dnn_feature_columns=deep_columns,
                     n_classes=label_cnt,  # 0.11 bug
-                    dnn_hidden_units=hidden_layers_value
-                )
+                    dnn_hidden_units=hidden_layers_value)
             elif FLAGS.model_type == "wide":
-                print("###################wide network#######################")
+                utils.tfmsa_logger("((1-1 wide Selected)############)")
                 m = tf.contrib.learn.LinearClassifier(model_dir=model_dir,
                                                       feature_columns=wide_columns
                                                       ,enable_centered_bias = True)
-                print("wide network end")
             elif FLAGS.model_type =="deep":
-                print("###################deep###############################")
-                print(deep_columns)
+                utils.tfmsa_logger("((1-1 deep Selected)############)")
                 m = tf.contrib.learn.DNNClassifier(model_dir=model_dir,
                                                        feature_columns=deep_columns,
                                                        n_classes = label_cnt, #0.11 bug
                                                        hidden_units=hidden_layers_value)
 
             rv = self.network_update(nnid,model_dir)
-            print("((1.Make WDN Network Build)) wdnn directory info update sucess")
+            utils.tfmsa_logger("((1.Make WDN Network Build)) wdnn directory info update sucess")
             return m
         except Exception as e:
             print("Error Message : {0}".format(e))
@@ -316,14 +220,8 @@ class WdnnCommonManager:
             :param nnid
             :param model_dir : directory of chkpoint of wdnn model
         """
-
-        utils.tfmsa_logger("((1.Make WDN Network Build)) start wddd build (" + nnid + ")")
-
         json_string = netconf.load_ori_format(nnid)
         json_object = json.loads(json_string)
-        #json_object = json.loads(json_string["datadesc"])
-        print("get json string in wdnn builder ####")
-        # load NN conf form db
         utils.tfmsa_logger("[4]load net conf form db")
 
         conf = netconf.load_conf(nnid)
@@ -331,20 +229,16 @@ class WdnnCommonManager:
         result_temp = netconf.get_network_config(nnid)
         label_cnt = json.loads(json.dumps(result_temp))
         label_object  = label_cnt["datasets"]
-        print(label_object)
-        # hidden_layers_value2 = conf["layer"]
-        # print("((1.Make WDN Network Build)) config load " + str(hidden_layers_value2))
-        print("((1.Make WDN Network Build)) set up Hidden Layers (" + str(hidden_layers_value), 'utf-8' + ")")
+        utils.tfmsa_logger("((1.Make WDN Network Build)) set up Hidden Layers (" + str(hidden_layers_value) + ")")
         return conf, hidden_layers_value, json_object, label_object
 
     def network_update(self,nnid, model_dir):
-        """ Wide Deep Network Info directory sae
+        """ Wide Deep Network update model directory
             :param nnid
             :param model_dir : directory of chkpoint of wdnn model
         """
         try:
             jd = jc.load_obj_json("{}")
-            #temporaly use dataset.
             jd.query = model_dir
             jd.nn_id = nnid
             netconf.update_network(jd)
